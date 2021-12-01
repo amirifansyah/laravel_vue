f0a94ceece9d43f72a33a4c278328956ac4061d2
amirifansyah <amirifansyah5@gmail.com>
2021-12-01 23:51:27 +0700

    first commit

---
+++ resources/js/components/products/home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h2 class="text-center">Home</h2>
+        <div class="row">
+            <div class="col-md-12 text-center">
+               <h2>
+                   ini home cuiiii
+               </h2>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/routes.js
@@ -0,0 +1,27 @@
+import ProductIndex from './components/products/index.vue';
+import ProductCreate from './components/products/create.vue';
+import ProductEdit from './components/products/edit.vue';
+import Home from './components/products/home.vue';
+
+export const routes = [
+    {
+        path: '/products',
+        component: ProductIndex,
+        name: "ProductIndex"
+    },
+    {
+        path: '/products/create',
+        component: ProductCreate,
+        name: "ProductCreate"
+    },
+    {
+        path: '/products/edit/:id',
+        component: ProductEdit,
+        name: "ProductEdit"
+    },
+    {
+        path: '/home',
+        component: Home,
+        name: "Home"
+    }
+];
